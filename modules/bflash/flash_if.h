76a8f51e6db18cea2ed02d1da55e1ff2d7faa06f
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2016-04-04 22:05:54 +0200

    Jedec address command corrected (Solve the MX29L3211 detection).

--- modules/bflash/flash_if.h
+++ modules/bflash/flash_if.h
@@ -26,8 +26,9 @@
 #define ADDR_UNLOCK_2_WM                0x02AA
 
 /* JEDEC */
-#define ADDR_UNLOCK_1_JEDEC             0x5555
-#define ADDR_UNLOCK_2_JEDEC             0x2AAA
+/* words to put on A14-A0 without A-1 (Q15) ! -> The address need to be shifted. */
+#define ADDR_UNLOCK_1_JEDEC             (0x5555<<1)
+#define ADDR_UNLOCK_2_JEDEC             (0x2AAA<<1)
 
 #define CMD_UNLOCK_DATA_1               0x00AA
 #define CMD_UNLOCK_DATA_2               0x0055
