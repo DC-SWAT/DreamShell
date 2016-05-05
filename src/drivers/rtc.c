fa1729150e688614499d2a71c52f6f6ded62523e
Ruslan Rostovtsev <rrostovtsev@ac-sw.com>
2016-05-05 10:27:27 +0600

    Improved AICA RTC driver

--- src/drivers/rtc.c
+++ src/drivers/rtc.c
@@ -1,7 +1,8 @@
 /* DreamShell ##version##
 
    rtc.c
-   Copyright (C) 2015 SWAT
+   Copyright (C) 2015-2016 SWAT
+   Copyright (C) 2016 Megavolt85
 */
 
 #include <arch/types.h>
@@ -15,42 +16,89 @@
 
 /* The AICA RTC is represented by a 32-bit seconds counter stored in 2 16-bit
    registers.*/
-#define AICA_RTC_SECS_H 0xa0710000
-#define AICA_RTC_SECS_L 0xa0710004
+#define AICA_RTC_SECS_H   0xa0710000
+#define AICA_RTC_SECS_L   0xa0710004
+#define AICA_RTC_WRITE_EN 0xa0710008
 
+#define AICA_RTC_ATTEMPTS_COUNT 10
 
-void rtc_gettimeofday(struct timeval *tv) {
-	
-	uint32 val1, val2;
+time_t rtc_gettime() {
+
+	time_t val1, val2;
+	int i = 0;
 
 	do {
-		
+
 		val1 = ((g2_read_32(AICA_RTC_SECS_H) & 0xffff) << 16) | (g2_read_32(AICA_RTC_SECS_L) & 0xffff);
 		val2 = ((g2_read_32(AICA_RTC_SECS_H) & 0xffff) << 16) | (g2_read_32(AICA_RTC_SECS_L) & 0xffff);
-		
+
+		if(i++ > AICA_RTC_ATTEMPTS_COUNT) {
+			return (-1);
+		}
+
 	} while (val1 != val2);
 
-	tv->tv_sec = val1 - TWENTY_YEARS;
-
-	/* Can't get microseconds with just a seconds counter. */
-	tv->tv_usec = 0;
+	return (val1 - TWENTY_YEARS);
 }
 
 
-int rtc_settimeofday(const struct timeval *tv) {
-	
-	uint32 val1, val2;
-	uint32 secs = tv->tv_sec + TWENTY_YEARS;
+int rtc_settime(time_t time) {
+
+	time_t val1, val2;
+	time_t secs = time + TWENTY_YEARS;
+	int i = 0;
 
 	do {
-		
+
+		g2_write_32(1, AICA_RTC_WRITE_EN);
 		g2_write_32((secs & 0xffff0000) >> 16, AICA_RTC_SECS_H);
 		g2_write_32((secs & 0xffff), AICA_RTC_SECS_L);
 
 		val1 = ((g2_read_32(AICA_RTC_SECS_H) & 0xffff) << 16) | (g2_read_32(AICA_RTC_SECS_L) & 0xffff);
 		val2 = ((g2_read_32(AICA_RTC_SECS_H) & 0xffff) << 16) | (g2_read_32(AICA_RTC_SECS_L) & 0xffff);
-		
-	} while (val1 != val2);
+
+		if(i++ > AICA_RTC_ATTEMPTS_COUNT) {
+			return (-1);
+		}
+
+	} while ((val1 != val2) && ((val1 & 0xffffff80) != (secs & 0xffffff80)));
 
 	return 0;
+}
+
+
+int rtc_gettimeutc(const struct tm *time) {
+
+	time_t timestamp = rtc_gettime();
+
+	if(time == NULL || timestamp == -1) {
+		return -1;
+	}
+
+	localtime_r(&timestamp, (struct tm*)time);
+	return 0;
+}
+
+
+int rtc_settimeutc(const struct tm *time) {
+
+	time_t timestamp = mktime((struct tm *)time);
+
+	if(time == NULL || timestamp == -1) {
+		return -1;
+	}
+
+	return rtc_settime(timestamp);
+}
+
+
+void rtc_gettimeofday(struct timeval *tv) {
+	tv->tv_sec = rtc_gettime();
+
+	/* Can't get microseconds with just a seconds counter. */
+	tv->tv_usec = 0;
+}
+
+int rtc_settimeofday(const struct timeval *tv) {
+	return rtc_settime(tv->tv_sec);
 }
