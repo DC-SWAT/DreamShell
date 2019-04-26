c243f11f035806b18bcf0c0f05a889c943d1ce26
SWAT <swat@211.ru>
2019-04-26 15:54:50 +0700

    Improved ping command

--- commands/ping/main.c
+++ commands/ping/main.c
@@ -2,39 +2,51 @@
 
    main.c - Ping
    Copyright (C) 2009 Lawrence Sebald
-   Copyright (C) 2009-2014 SWAT
+   Copyright (C) 2009-2019 SWAT
 
 */
-            
 #include "ds.h"
-
+#include <netdb.h>
 
 #define DATA_SIZE 56
 
-/*
-static void icmp_ds_echo_cb(const uint8 *ip, uint16 seq, uint64 delta_us,
-                                 uint8 ttl, const uint8* data, int data_sz) {
-    ds_printf(" %d bytes from %d.%d.%d.%d: icmp_seq=%d ttl=%d time=%.3f ms\n",
-           data_sz, ip[0], ip[1], ip[2], ip[3], seq, ttl,
-           delta_us / 1000.0);
-}*/
+int main(int argc, char *argv[]) {
 
-
-int main(int argc, char *argv[]) { 
-     
     if(argc < 2) {
-		ds_printf("Usage: %s ip_addr\n", argv[0]);
-        return CMD_NO_ARG; 
-    } 
+		ds_printf("Usage: %s host\n", argv[0]);
+        return CMD_NO_ARG;
+    }
 
-	//net_icmp_echo_cb = icmp_ds_echo_cb;
-
-	/* The address to ping... */
-    uint8 addr[4] = { 192, 168, 0, 1 };
+    uint8 ip[4];
+    int i, scan_ip[4] = { 0, 0, 0 ,0 };
     uint8 data[DATA_SIZE];
-    int i;
+    char *name = argv[1];
+    struct hostent *hent;
 
-	sscanf(argv[1], "%c.%c.%c.%c", &addr[0], &addr[1], &addr[2], &addr[3]);
+    i = sscanf(name, "%d.%d.%d.%d", &scan_ip[0], &scan_ip[1], &scan_ip[2], &scan_ip[3]);
+
+    if (i < 4) {
+        hent = gethostbyname((const char *)name);
+
+        if(hent) {
+            name = hent->h_addr;
+            ds_printf("DS_OK: %s\n", hent->h_name);
+        } else {
+            ds_printf("DS_ERROR: Can't lookup host %s\n", name);
+            return CMD_ERROR;
+        }
+
+        i = sscanf(name, "%d.%d.%d.%d", &scan_ip[0], &scan_ip[1], &scan_ip[2], &scan_ip[3]);
+
+        if (i < 4) {
+            ds_printf("DS_ERROR: Bad format: %d %s\n", i, name);
+            return CMD_ERROR;
+        }
+    }
+
+    for(i = 0; i < 4; ++i) {
+        ip[i] = (uint8)(scan_ip[i] & 0xff);
+    }
 
     /* Fill in the data for the ping packet... this is pretty simple and doesn't
        really have any real meaning... */
@@ -42,15 +54,17 @@ int main(int argc, char *argv[]) {
         data[i] = (uint8)i;
     }
 
+    ds_printf("DS_PROCESS: Connecting to %d.%d.%d.%d\n", ip[0], ip[1], ip[2], ip[3]);
+
     /* Send out 4 pings, waiting 250ms between attempts. */
     for(i = 0; i < 4; ++i) {
-        net_icmp_send_echo(net_default_dev, addr, 0, i, data, DATA_SIZE);
+        if (net_icmp_send_echo(net_default_dev, ip, 0, i, data, DATA_SIZE) < 0) {
+            ds_printf("DS_ERROR: send echo error %d\n", errno);
+            return CMD_ERROR;
+        }
         thd_sleep(250);
     }
 
-	//net_icmp_echo_cb = icmp_default_echo_cb;
-
-    return CMD_OK; 
+    ds_printf("DS_OK: Done.\n");
+    return CMD_OK;
 }
-
-
