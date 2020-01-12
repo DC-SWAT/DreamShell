632b74506d5449b41bab83c4b08f98bb665fb903
cpasjuste <cpasjuste@gmail.com>
2020-01-12 20:24:51 +0100

    ubuntu: add compile script

---
+++ sdk/toolchain/ubuntu_build.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/bash
+
+# exports
+export SH_PREFIX=/opt/toolchains/dc/sh-elf
+export ARM_PREFIX=/opt/toolchains/dc/arm-eabi
+export KOS_ROOT=/usr/local/dc/kos
+export KOS_BASE=$KOS_ROOT/kos
+export KOS_PORTS=$KOS_ROOT/kos-ports
+
+# install deps and setup directories
+sudo apt-get update
+sudo apt-get install -y subversion build-essential gcc-4.7 make
+	autoconf bison flex libelf-dev texinfo latex2html \
+	git wget sed lyx libjpeg62-dev libpng-dev
+
+sudo mkdir -p $KOS_ROOT
+sudo mkdir -p $SH_PREFIX/sh-elf/include
+sudo mkdir -p $ARM_PREFIX/share
+sudo chown -R $USER:$USER $KOS_ROOT
+sudo chown -R $USER:$USER $SH_PREFIX
+sudo chown -R $USER:$USER $KOS_ROOT
+
+# clone kos
+git clone git://git.code.sf.net/p/cadcdev/kallistios $KOS_BASE
+git clone --recursive git://git.code.sf.net/p/cadcdev/kos-ports $KOS_PORTS
+
+# prepare
+./download.sh
+./unpack.sh
+cd gcc-5.2.0
+./contrib/download_prerequisites
+make patch
+
+# build
+make -j9 build-sh4-binutils
+make -j9 build-sh4-gcc-pass1
+make -j9 build-sh4-newlib-only
+make -j9 fixup-sh4-newlib
+make -j9 build-sh4-gcc-pass2
+make -j9 build-arm-binutils
+make -j9 build-arm-gcc
+
+# env script
+cp $KOS_BASE/doc/environ.sh.sample $KOS_BASE/environ.sh
+sed -i 's/\/opt\/toolchains\/dc\/kos/\/usr\/local\/dc\/kos\/kos/g' $KOS_BASE/environ.sh
+source $KOS_BASE/environ.sh
+
+# build kos and kos-ports
+pushd `pwd`
+cd $KOS_BASE
+make -j9
+cd $KOS_PORTS
+./utils/build-all.sh
+popd
