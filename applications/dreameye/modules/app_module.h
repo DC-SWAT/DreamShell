2558cf489a7b0910608ee7eb0a9e610b1bdc3a35
DC-SWAT <swat@211.ru>
2024-01-04 19:58:54 +0700

    [app-dreameye] Windowed preview.

--- applications/dreameye/modules/app_module.h
+++ applications/dreameye/modules/app_module.h
@@ -1,12 +1,16 @@
 /* DreamShell ##version##
 
    app_module.h - DreamEye app module header
-   Copyright (C) 2023 SWAT
+   Copyright (C) 2023, 2024 SWAT
 */
 
 #include <ds.h>
 
 void DreamEyeApp_Init(App_t *app);
+
+void DreamEyeApp_Shutdown(App_t *app);
+
+void DreamEyeApp_Open(App_t *app);
 
 void DreamEyeApp_ShowPage(GUI_Widget *widget);
 
--- applications/dreameye/modules/module.c
+++ applications/dreameye/modules/module.c
@@ -1,7 +1,7 @@
 /* DreamShell ##version##
 
    module.c - DreamEye app module
-   Copyright (C) 2023 SWAT 
+   Copyright (C) 2023, 2024 SWAT 
 */
 
 #include <ds.h>
@@ -14,29 +14,61 @@ static struct {
 	App_t *app;
 	GUI_Widget *pages;
 
+	int preview_isp_mode;
+	int preview_fullscreen;
+	int preview_x;
+	int preview_y;
+	int preview_scale;
+
 } self;
+
+static void ShowCameraPreview() {
+	maple_device_t *dev = maple_enum_type(0, MAPLE_FUNC_CAMERA);
+	dreameye_preview_shutdown();
+	if(dev) {
+		dreameye_preview_init(dev, self.preview_isp_mode,
+			self.preview_fullscreen, self.preview_scale,
+			self.preview_x, self.preview_y);
+	} else {
+		ds_printf("DS_ERROR: No camera present\n");
+	}
+}
 
 void DreamEyeApp_Init(App_t *app) {
 	self.app = app;
 	self.pages = APP_GET_WIDGET("pages");
+
+	self.preview_isp_mode = DREAMEYE_ISP_MODE_QSIF;
+	self.preview_fullscreen = 0;
+	self.preview_scale = 2;
+	self.preview_x = 160;
+	self.preview_y = 220;
+}
+
+void DreamEyeApp_Shutdown(App_t *app) {
+	(void)app;
+	dreameye_preview_shutdown();
+}
+
+void DreamEyeApp_Open(App_t *app) {
+	(void)app;
+	ShowCameraPreview();
 }
 
 void DreamEyeApp_ShowPage(GUI_Widget *widget) {
+	(void)widget;
 }
 
 void DreamEyeApp_ShowPreview(GUI_Widget *widget) {
 	const char *object_name = GUI_ObjectGetName((GUI_Object *)widget);
-	maple_device_t *dev = maple_enum_type(0, MAPLE_FUNC_CAMERA);
-	int isp_mode = DREAMEYE_ISP_MODE_SIF;
-
-	if(!dev) {
-		ds_printf("DS_ERROR: No camera present\n");
-		return;
-	}
 
 	if(object_name[8] == '1') {
-		isp_mode = DREAMEYE_ISP_MODE_QSIF;
+		self.preview_isp_mode = DREAMEYE_ISP_MODE_QSIF;
+		self.preview_scale = 2;
+	} else {
+		self.preview_isp_mode = DREAMEYE_ISP_MODE_SIF;
+		self.preview_scale = 1;
 	}
 
-	dreameye_preview_init(dev, isp_mode);
+	ShowCameraPreview();
 }
