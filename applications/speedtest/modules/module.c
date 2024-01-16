9b0bc8ca83c31776de123342b56efe5ab5fa3e80
DC-SWAT <swat@211.ru>
2024-01-16 23:34:02 +0700

    [app-speed] Added DMA IO read test and use ns timer. Also a new icon for app and changed font color for results.

--- applications/speedtest/modules/module.c
+++ applications/speedtest/modules/module.c
@@ -2,7 +2,7 @@
 	
 	module.c - Speedtest app module
 	Copyright (C)2014 megavolt85
-	Copyright (C)2014-2020 SWAT
+	Copyright (C)2014-2020, 2024 SWAT
 	
 	http://www.dc-swat.ru
 */
@@ -19,6 +19,7 @@ static struct {
 	GUI_Widget *pc_c;
 	GUI_Widget *speedwr;
 	GUI_Widget *speedrd;
+	GUI_Widget *speedrdd;
 	GUI_Widget *status;
 } self;
 
@@ -35,6 +36,60 @@ static void show_status_error(char *msg) {
 	GUI_LabelSetText(self.status, msg);
 }
 
+int test_ide_dma(void) {
+	kos_blockdev_t bd_dma;
+	uint64 sdma, edma;
+	uint32_t tm;
+	uint8_t pt;
+	uint8_t *buf;
+	size_t buf_size = 512 * 1024;
+	double speed;
+	char result[256];
+
+	buf = memalign(32, buf_size);
+
+	if(!buf) {
+		return -1;
+	}
+	if(g1_ata_blockdev_for_partition(0, 1, &bd_dma, &pt)) {
+		dbglog(DBG_DEBUG, "Couldn't get DMA blockdev for partition!\n");
+		free(buf);
+		return -1;
+	}
+	show_status_ok("Testing DMA IO read speed...");
+	GUI_LabelSetText(self.speedrdd, "...");
+
+	ShutdownVideoThread();
+	sdma = timer_ns_gettime64();
+
+	if(bd_dma.read_blocks(&bd_dma, 0, 1024, buf)) {
+		dbglog(DBG_DEBUG, "couldn't read block by DMA: %s\n", strerror(errno));
+		free(buf);
+		return -1;
+	}
+	edma = timer_ns_gettime64();
+	tm = (edma - sdma) / 1000000;
+	InitVideoThread();
+
+	free(buf);
+	speed = buf_size / ((float)tm / 1000);
+
+	snprintf(result, sizeof(result), 
+		"DMA IO read: %.2f Kbytes/s (%.2f Mbit/s)",
+		speed / 1024, ((speed / 1024) / 1024) * 8);
+	GUI_LabelSetText(self.speedrdd, result);
+	show_status_ok("Complete!"); 
+
+	ds_printf("DS_OK: Complete!\n"
+		" Test: DMA IO read\n Time: %ld ms\n"
+		" Speed: %.2f Kbytes/s (%.2f Mbit/s)\n"
+		" Size: %d Kb\n",
+		tm, speed / 1024, 
+		((speed / 1024) / 1024) * 8, 
+		buf_size / 1024);
+
+	return 0;
+}
 
 void Speedtest_Run(GUI_Widget *widget) {
 
@@ -45,13 +100,20 @@ void Speedtest_Run(GUI_Widget *widget) {
 	uint32 t;
 	double speed;
 	file_t fd;
-	int read_only = 0;
+	int read_only = 0, is_ide = 0;
 	
 	char name[64];
-	char result[128];
+	char result[256];
 	const char *wname = GUI_ObjectGetName((GUI_Object *)widget);
-	
-	if(!strncasecmp(wname, "/cd", 3)) {
+
+	GUI_LabelSetText(self.speedwr, "   ");
+	GUI_LabelSetText(self.speedrd, "   ");
+	GUI_LabelSetText(self.speedrdd, "   ");
+
+	if(!strncmp(wname, "/ide", 4)) {
+		is_ide = 1;
+	}
+	if(!strncmp(wname, "/cd", 3)) {
 		
 		read_only = 1;
 		snprintf(name, sizeof(name), "%s/1DS_CORE.BIN", wname);
@@ -64,9 +126,8 @@ void Speedtest_Run(GUI_Widget *widget) {
 		}
 	}
 	
-	show_status_ok("Testing WRITE speed...");
+	show_status_ok("Testing PIO FS wite speed...");
 	GUI_LabelSetText(self.speedwr, "...");
-	GUI_LabelSetText(self.speedrd, "   ");
 	
 	snprintf(name, sizeof(name), "%s/%s.tst", wname, lib_get_name());
 	
@@ -75,7 +136,7 @@ void Speedtest_Run(GUI_Widget *widget) {
 	}
 	
 	/* WRITE TEST */
-	fd = fs_open(name, O_CREAT | O_WRONLY);
+	fd = fs_open(name, O_WRONLY | O_CREAT | O_TRUNC);
 	
 	if (fd == FILEHND_INVALID) {
 		ds_printf("DS_ERROR: Can't open %s for write: %d\n", name, errno);
@@ -84,7 +145,7 @@ void Speedtest_Run(GUI_Widget *widget) {
 	}
 
 	ShutdownVideoThread(); 
-	time_before = timer_ms_gettime64();
+	time_before = timer_ns_gettime64();
 	
 	while(cnt < size) {
 		
@@ -101,43 +162,42 @@ void Speedtest_Run(GUI_Widget *widget) {
 		buff += rs;
 		cnt += rs;
 	}
-	
-	time_after = timer_ms_gettime64();
+
+	time_after = timer_ns_gettime64();
 	InitVideoThread();
-	
-	t = (uint32)(time_after - time_before);
+
+	t = (uint32)(time_after - time_before) / 1000000;
 	speed = size / ((float)t / 1000);
 	fs_close(fd);
-	
+
 	snprintf(result, sizeof(result), 
-				"Write speed %.2f Kbytes/s (%.2f Mbit/s) Time: %ld ms",
-				speed / 1024, ((speed / 1024) / 1024) * 8, t);
-	
+		"PIO FS write: %.2f Kbytes/s (%.2f Mbit/s)",
+		speed / 1024, ((speed / 1024) / 1024) * 8);
+
 	GUI_LabelSetText(self.speedwr, result);
-	show_status_ok("Complete!"); 
-	
+	show_status_ok("Complete!");
+
 	ds_printf("DS_OK: Complete!\n"
-				" Test: write\n Time: %ld ms\n"
-				" Speed: %.2f Kbytes/s (%.2f Mbit/s)\n"
-				" Size: %d Kb\n Buff: %d Kb\n", 
-				t, speed / 1024, 
-				((speed / 1024) / 1024) * 8, 
-				size / 1024, buff_size / 1024); 
-				
+		" Test: PIO FS write\n Time: %ld ms\n"
+		" Speed: %.2f Kbytes/s (%.2f Mbit/s)\n"
+		" Size: %d Kb\n Buff: %d Kb\n", 
+		t, speed / 1024, 
+		((speed / 1024) / 1024) * 8, 
+		size / 1024, buff_size / 1024);
+
 readtest:
 
-	show_status_ok("Testing READ speed...");
+	show_status_ok("Testing PIO FS read speed...");
 	GUI_LabelSetText(self.speedrd, "...");
 
 	/* READ TEST */
 	fd = fs_open(name, O_RDONLY);
-	
+
 	if (fd == FILEHND_INVALID) {
 		ds_printf("DS_ERROR: Can't open %s for read: %d\n", name, errno);
 		show_status_error("Can't open file for read");
 		return;
 	}
-
 	if(read_only) {
 		GUI_LabelSetText(self.speedwr, "Write test passed");
 		/* Reset ISO9660 filesystem cache */
@@ -145,19 +205,19 @@ readtest:
 		fs_close(fd);
 		fd = fs_open(name, O_RDONLY);
 	}
-	
+
 	time_before = time_after = t = cnt = 0;
 	speed = 0.0f;
 	size = fs_total(fd);
 	buff = rd_buff;
 
 	ShutdownVideoThread();
-	time_before = timer_ms_gettime64();
+	time_before = timer_ns_gettime64();
 
 	while(cnt < size) {
-		
+
 		rs = fs_read(fd, buff, buff_size);
-		
+
 		if(rs <= 0) {
 			fs_close(fd);
 			InitVideoThread(); 
@@ -165,52 +225,57 @@ readtest:
 			show_status_error("Can't read file");
 			return;
 		}
-		
 		cnt += rs;
 	}
-	
-	time_after = timer_ms_gettime64();
-	t = (uint32)(time_after - time_before);
+
+	time_after = timer_ns_gettime64();
+	t = (uint32)(time_after - time_before) / 1000000;
 	speed = size / ((float)t / 1000);
+
 	fs_close(fd);
-	
+
 	if(!read_only) { 
 		fs_unlink(name);
 	} else {
 		cdrom_spin_down();
 	}
-	
+
 	snprintf(result, sizeof(result), 
-			"Read speed %.2f Kbytes/s (%.2f Mbit/s) Time: %ld ms",
-			speed / 1024, ((speed / 1024) / 1024) * 8, t);
-	
+		"PIO FS read: %.2f Kbytes/s (%.2f Mbit/s)",
+		speed / 1024, ((speed / 1024) / 1024) * 8);
+
 	InitVideoThread();
-	
+
 	ds_printf("DS_OK: Complete!\n"
-				" Test: read\n Time: %ld ms\n"
-				" Speed: %.2f Kbytes/s (%.2f Mbit/s)\n"
-				" Size: %d Kb\n Buff: %d Kb\n", 
-				t, speed / 1024, 
-				((speed / 1024) / 1024) * 8, 
-				size / 1024, buff_size / 1024);
-    
+		" Test: PIO FS read\n Time: %ld ms\n"
+		" Speed: %.2f Kbytes/s (%.2f Mbit/s)\n"
+		" Size: %d Kb\n Buff: %d Kb\n", 
+		t, speed / 1024, 
+		((speed / 1024) / 1024) * 8, 
+		size / 1024, buff_size / 1024);
+
 	GUI_LabelSetText(self.speedrd, result);
 	show_status_ok("Complete!"); 
+
+	if(is_ide) {
+		test_ide_dma();
+	}
 }
 
 
 void Speedtest_Init(App_t *app) {
-	
+
 	memset(&self, 0, sizeof(self));
 
 	if(app != NULL) {
-		
+
 		self.app = app;
 
 		self.speedrd = APP_GET_WIDGET("speedr_text");
+		self.speedrdd = APP_GET_WIDGET("speedrd_text");
 		self.speedwr = APP_GET_WIDGET("speedw_text");
 		self.status  = APP_GET_WIDGET("status_text");
-		
+
 		self.cd_c  = APP_GET_WIDGET("/cd");
 		self.sd_c  = APP_GET_WIDGET("/sd");
 		self.hdd_c = APP_GET_WIDGET("/ide");
@@ -222,7 +287,7 @@ void Speedtest_Init(App_t *app) {
 			GUI_WidgetSetEnabled(self.cd_c, 0);
 		}
 		if(!DirExists("/ide")) GUI_WidgetSetEnabled(self.hdd_c, 0);
-		
+
 	} else {
 		ds_printf("DS_ERROR: %s: Attempting to call %s is not by the app initiate.\n", 
 					lib_get_name(), __func__);
