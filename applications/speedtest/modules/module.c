eac07535365a8f65995c37bc587927dc9b68cd17
DC-SWAT <swat@211.ru>
2024-07-10 13:23:35 +0700

    [app] Renaming tests in the Speedtest.

--- applications/speedtest/modules/module.c
+++ applications/speedtest/modules/module.c
@@ -18,9 +18,9 @@ static struct {
 	GUI_Widget *sd_c;
 	GUI_Widget *hdd_c;
 	GUI_Widget *pc_c;
-	GUI_Widget *pio_write_text;
-	GUI_Widget *pio_read_text;
-	GUI_Widget *dma_read_text;
+	GUI_Widget *fs_write_text;
+	GUI_Widget *fs_read_text;
+	GUI_Widget *io_read_text;
 	GUI_Widget *status;
 } self;
 
@@ -37,7 +37,7 @@ static void show_status_error(char *msg) {
 	GUI_LabelSetText(self.status, msg);
 }
 
-int test_ide_dma_io(void) {
+int test_ide_io_io(void) {
 	kos_blockdev_t bdev;
 	uint64 st, et;
 	uint32_t tm;
@@ -58,8 +58,8 @@ int test_ide_dma_io(void) {
 		free(buf);
 		return -1;
 	}
-	show_status_ok("Testing DMA IO read speed...");
-	GUI_LabelSetText(self.dma_read_text, "...");
+	show_status_ok("Testing IO read speed...");
+	GUI_LabelSetText(self.io_read_text, "...");
 
 	ShutdownVideoThread();
 	st = timer_ns_gettime64();
@@ -77,13 +77,13 @@ int test_ide_dma_io(void) {
 	speed = buf_size / ((float)tm / 1000);
 
 	snprintf(result, sizeof(result), 
-		"DMA IO read: %.2f Kbytes/s or %.2f Mbit/s",
+		"IO read: %.2f Kbytes/s or %.2f Mbit/s",
 		speed / 1024, ((speed / 1024) / 1024) * 8);
-	GUI_LabelSetText(self.dma_read_text, result);
+	GUI_LabelSetText(self.io_read_text, result);
 	show_status_ok("Complete!"); 
 
 	ds_printf("DS_OK: Complete!\n"
-		" Test: DMA IO read\n Time: %ld ms\n"
+		" Test: IO read\n Time: %ld ms\n"
 		" Speed: %.2f Kbytes/s (%.2f Mbit/s)\n"
 		" Size: %d Kb\n",
 		tm, speed / 1024, 
@@ -115,7 +115,7 @@ int test_sd_io(void) {
 		return -1;
 	}
 	show_status_ok("Testing SD IO read speed...");
-	GUI_LabelSetText(self.dma_read_text, "...");
+	GUI_LabelSetText(self.io_read_text, "...");
 
 	ShutdownVideoThread();
 	st = timer_ns_gettime64();
@@ -135,7 +135,7 @@ int test_sd_io(void) {
 	snprintf(result, sizeof(result), 
 		"SD IO read: %.2f Kbytes/s or %.2f Mbit/s",
 		speed / 1024, ((speed / 1024) / 1024) * 8);
-	GUI_LabelSetText(self.dma_read_text, result);
+	GUI_LabelSetText(self.io_read_text, result);
 	show_status_ok("Complete!"); 
 
 	ds_printf("DS_OK: Complete!\n"
@@ -164,9 +164,9 @@ void Speedtest_Run(GUI_Widget *widget) {
 	char result[256];
 	const char *wname = GUI_ObjectGetName((GUI_Object *)widget);
 
-	GUI_LabelSetText(self.pio_write_text, " ");
-	GUI_LabelSetText(self.pio_read_text, " ");
-	GUI_LabelSetText(self.dma_read_text, " ");
+	GUI_LabelSetText(self.fs_write_text, " ");
+	GUI_LabelSetText(self.fs_read_text, " ");
+	GUI_LabelSetText(self.io_read_text, " ");
 
 	if(!strncmp(wname, "/ide", 4)) {
 		is_ide = 1;
@@ -187,8 +187,8 @@ void Speedtest_Run(GUI_Widget *widget) {
 		}
 	}
 
-	show_status_ok("Testing PIO FS write speed...");
-	GUI_LabelSetText(self.pio_write_text, "...");
+	show_status_ok("Testing FS write speed...");
+	GUI_LabelSetText(self.fs_write_text, "...");
 
 	snprintf(name, sizeof(name), "%s/%s.tst", wname, lib_get_name());
 
@@ -231,14 +231,14 @@ void Speedtest_Run(GUI_Widget *widget) {
 	fs_close(fd);
 
 	snprintf(result, sizeof(result), 
-		"PIO FS write: %.2f Kbytes/s or %.2f Mbit/s",
+		"FS write: %.2f Kbytes/s or %.2f Mbit/s",
 		speed / 1024, ((speed / 1024) / 1024) * 8);
 
-	GUI_LabelSetText(self.pio_write_text, result);
+	GUI_LabelSetText(self.fs_write_text, result);
 	show_status_ok("Complete!");
 
 	ds_printf("DS_OK: Complete!\n"
-		" Test: PIO FS write\n Time: %ld ms\n"
+		" Test: FS write\n Time: %ld ms\n"
 		" Speed: %.2f Kbytes/s (%.2f Mbit/s)\n"
 		" Size: %d Kb\n Buff: %d Kb\n", 
 		t, speed / 1024, 
@@ -247,8 +247,8 @@ void Speedtest_Run(GUI_Widget *widget) {
 
 readtest:
 
-	show_status_ok("Testing PIO FS read speed...");
-	GUI_LabelSetText(self.pio_read_text, "...");
+	show_status_ok("Testing FS read speed...");
+	GUI_LabelSetText(self.fs_read_text, "...");
 
 	/* READ TEST */
 	fd = fs_open(name, O_RDONLY);
@@ -259,7 +259,7 @@ readtest:
 		return;
 	}
 	if(read_only) {
-		GUI_LabelSetText(self.pio_write_text, "Write test passed");
+		GUI_LabelSetText(self.fs_write_text, "Write test passed");
 		/* Reset ISO9660 filesystem cache */
 		fs_ioctl(fd, 0, NULL);
 		fs_close(fd);
@@ -301,24 +301,24 @@ readtest:
 	}
 
 	snprintf(result, sizeof(result), 
-		"PIO FS read: %.2f Kbytes/s or %.2f Mbit/s",
+		"FS read: %.2f Kbytes/s or %.2f Mbit/s",
 		speed / 1024, ((speed / 1024) / 1024) * 8);
 
 	InitVideoThread();
 
 	ds_printf("DS_OK: Complete!\n"
-		" Test: PIO FS read\n Time: %ld ms\n"
+		" Test: FS read\n Time: %ld ms\n"
 		" Speed: %.2f Kbytes/s (%.2f Mbit/s)\n"
 		" Size: %d Kb\n Buff: %d Kb\n", 
 		t, speed / 1024, 
 		((speed / 1024) / 1024) * 8, 
 		size / 1024, buff_size / 1024);
 
-	GUI_LabelSetText(self.pio_read_text, result);
+	GUI_LabelSetText(self.fs_read_text, result);
 	show_status_ok("Complete!"); 
 
 	if(is_ide) {
-		test_ide_dma_io();
+		test_ide_io_io();
 	}
 	else if(is_sd) {
 		test_sd_io();
@@ -334,9 +334,9 @@ void Speedtest_Init(App_t *app) {
 
 		self.app = app;
 
-		self.pio_read_text = APP_GET_WIDGET("pio-read-text");
-		self.dma_read_text = APP_GET_WIDGET("dma-read-text");
-		self.pio_write_text = APP_GET_WIDGET("pio-write-text");
+		self.fs_read_text = APP_GET_WIDGET("fs-read-text");
+		self.io_read_text = APP_GET_WIDGET("io-read-text");
+		self.fs_write_text = APP_GET_WIDGET("fs-write-text");
 		self.status  = APP_GET_WIDGET("status-text");
 
 		self.cd_c  = APP_GET_WIDGET("/cd");
