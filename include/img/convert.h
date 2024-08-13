d11432e1937f4fb3c40f88192f2f3922f148f7a7
ManiacVera <maniac_vera@hotmail.com>
2024-08-13 16:29:26 -0600

    [core] - depure source

--- include/img/convert.h
+++ include/img/convert.h
@@ -3,8 +3,11 @@
 
 /* Read PRV and write PNG*/
 int pvr_to_png(const char *source_file, const char *dest_file, uint32 output_width, uint32 output_height, uint8 resolution);
+
+/* Read PRV and write JPG*/
 int pvr_to_jpg(const char *source_file, const char *dest_file, uint32 output_width, uint32 output_height, uint8 resolution);
-int zoom_jpg_file(const char *inFileName, const char *outFileName, float factor);
+
+/* Read PRV and resize*/
 unsigned char* resize_image(unsigned char *data, uint width, uint height, uint output_width, uint output_height, uint8 pixel_layout);
 
 #endif // _IMG_CONVERT_H
