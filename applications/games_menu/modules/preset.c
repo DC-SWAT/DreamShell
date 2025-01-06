8e7bcce32f57455721b513d23858a00768c57279
ManiacVera <maniac_vera@hotmail.com>
2025-01-05 22:08:31 -0600

    [app-games-menu] - changes to presets layout

--- applications/games_menu/modules/preset.c
+++ applications/games_menu/modules/preset.c
@@ -1509,7 +1509,7 @@ void ShowPresetMenu(int game_index)
 
 			Font *form_font = TSU_FontCreate(font_path, PVR_LIST_TR_POLY);
 
-			self.preset_menu_form = TSU_FormCreate(640/2 - 618/2, 480/2 + 450/2, 618, 455, true, 3, true, true, form_font, &OnViewIndexChangedEvent);
+			self.preset_menu_form = TSU_FormCreate(640/2 - 618/2, 480/2 + 450/2, 612, 455, true, 3, true, true, form_font, &OnViewIndexChangedEvent);
 			TSU_FormGetObjectsCurrentViewEvent(self.preset_menu_form, &OnGetObjectsCurrentViewEvent);
 
 			{
