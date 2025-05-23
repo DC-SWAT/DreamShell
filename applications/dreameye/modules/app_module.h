cbc5f4236873c187686aa5a02be58ce3e2803d76
HeadHunter2020 <88332038+HeadHunter2020@users.noreply.github.com>
2025-05-23 11:20:32 +0300

    [app-dreameye] Renamed DreamEye to Dreameye (#96)

--- applications/dreameye/modules/app_module.h
+++ applications/dreameye/modules/app_module.h
@@ -1,33 +1,33 @@
 /* DreamShell ##version##
 
-   app_module.h - DreamEye app module header
+   app_module.h - Dreameye app module header
    Copyright (C) 2023, 2024 SWAT
 */
 
 #include <ds.h>
 
-void DreamEyeApp_Init(App_t *app);
+void DreameyeApp_Init(App_t *app);
 
-void DreamEyeApp_Shutdown(App_t *app);
+void DreameyeApp_Shutdown(App_t *app);
 
-void DreamEyeApp_Open(App_t *app);
+void DreameyeApp_Open(App_t *app);
 
-void DreamEyeApp_ShowMainPage(GUI_Widget *widget);
+void DreameyeApp_ShowMainPage(GUI_Widget *widget);
 
-void DreamEyeApp_ShowPhotoPage(GUI_Widget *widget);
+void DreameyeApp_ShowPhotoPage(GUI_Widget *widget);
 
-void DreamEyeApp_ExportPhoto(GUI_Widget *widget);
+void DreameyeApp_ExportPhoto(GUI_Widget *widget);
 
-void DreamEyeApp_ErasePhoto(GUI_Widget *widget);
+void DreameyeApp_ErasePhoto(GUI_Widget *widget);
 
-void DreamEyeApp_FileBrowserItemClick(GUI_Widget *widget);
+void DreameyeApp_FileBrowserItemClick(GUI_Widget *widget);
 
-void DreamEyeApp_FileBrowserConfirm(GUI_Widget *widget);
+void DreameyeApp_FileBrowserConfirm(GUI_Widget *widget);
 
-void DreamEyeApp_ChangeResolution(GUI_Widget *widget);
+void DreameyeApp_ChangeResolution(GUI_Widget *widget);
 
-void DreamEyeApp_ToggleDetectQR(GUI_Widget *widget);
+void DreameyeApp_ToggleDetectQR(GUI_Widget *widget);
 
-void DreamEyeApp_ToggleExecQR(GUI_Widget *widget);
+void DreameyeApp_ToggleExecQR(GUI_Widget *widget);
 
-void DreamEyeApp_Abort(GUI_Widget *widget);
+void DreameyeApp_Abort(GUI_Widget *widget);
