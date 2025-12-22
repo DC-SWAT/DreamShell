83f4019950715dc2a4ce6e5e89f361ba902a4e50
DC-SWAT <swat@211.ru>
2025-12-22 17:52:38 +0700

    [app-network] Refactored network connection functions to toggle states. Improved button labeling and startup settings management. Added support for keeping servers running in the background.

--- applications/network/modules/app_module.h
+++ applications/network/modules/app_module.h
@@ -12,13 +12,13 @@ void NetworkApp_Shutdown(App_t *app);
 
 void NetworkApp_Open(App_t *app);
 
-void NetworkApp_ConnectBBA(GUI_Widget *widget);
+void NetworkApp_ToggleEthernet(GUI_Widget *widget);
 
-void NetworkApp_ConnectModem(GUI_Widget *widget);
+void NetworkApp_ToggleModem(GUI_Widget *widget);
 
-void NetworkApp_FTP(GUI_Widget *widget);
+void NetworkApp_ToggleFTP(GUI_Widget *widget);
 
-void NetworkApp_HTTP(GUI_Widget *widget);
+void NetworkApp_ToggleHTTP(GUI_Widget *widget);
 
 void NetworkApp_Exit(GUI_Widget *widget);
 
--- applications/network/modules/module.c
+++ applications/network/modules/module.c
@@ -6,6 +6,7 @@
 
 #include <ds.h>
 #include <stdbool.h>
+#include <kos/net.h>
 
 #include "app_module.h"
 
@@ -13,10 +14,10 @@ DEFAULT_MODULE_EXPORTS(app_network);
 
 typedef enum {
     APP_ACTION_IDLE,
-    APP_ACTION_CONNECT_BBA,
-    APP_ACTION_CONNECT_MODEM,
-    APP_ACTION_CONNECT_FTPD,
-    APP_ACTION_CONNECT_HTTPD
+    APP_ACTION_ETHERNET,
+    APP_ACTION_MODEM,
+    APP_ACTION_FTPD,
+    APP_ACTION_HTTPD
 } app_action_t;
 
 static struct {
@@ -25,6 +26,10 @@ static struct {
     char *last_ip;
     app_action_t action;
     Settings_t *settings;
+    bool is_eth_connected;
+    bool is_ppp_connected;
+    bool is_ftpd_started;
+    bool is_httpd_started;
 
     GUI_Widget *pages;
     GUI_Widget *dialog;
@@ -36,6 +41,14 @@ static struct {
     GUI_Widget *startup_connect_eth_but;
     GUI_Widget *startup_connect_ppp_but;
     GUI_Widget *startup_ntp_but;
+    GUI_Widget *keep_running_but;
+
+    GUI_Widget *eth_but_txt;
+    GUI_Widget *ppp_but_txt;
+    GUI_Widget *ftpd_but_txt;
+    GUI_Widget *httpd_but_txt;
+
+    Item_list_t *loaded_modules;
 
 } self;
 
@@ -49,6 +62,40 @@ static void SetupNetworkSettings() {
     if(self.startup_ntp_but) {
         GUI_WidgetSetState(self.startup_ntp_but, self.settings->network.startup_ntp);
     }
+    if(self.keep_running_but) {
+        GUI_WidgetSetState(self.keep_running_but, self.is_ftpd_started || self.is_httpd_started);
+    }
+}
+
+static void UpdateButtons() {
+    if(self.is_eth_connected) {
+        GUI_LabelSetText(self.eth_but_txt, "Disconnect Ethernet");
+    }
+    else {
+        GUI_LabelSetText(self.eth_but_txt, "Connect Ethernet");
+    }
+
+    if(self.is_ppp_connected) {
+        GUI_LabelSetText(self.ppp_but_txt, "Disconnect Modem");
+    }
+    else {
+        GUI_LabelSetText(self.ppp_but_txt, "Connect Modem");
+    }
+
+    if(self.is_ftpd_started) {
+        GUI_LabelSetText(self.ftpd_but_txt, "Stop FTP Server");
+    }
+    else {
+        GUI_LabelSetText(self.ftpd_but_txt, "Start FTP Server");
+    }
+
+    if(self.is_httpd_started) {
+        GUI_LabelSetText(self.httpd_but_txt, "Stop HTTP Server");
+    }
+    else {
+        GUI_LabelSetText(self.httpd_but_txt, "Start HTTP Server");
+    }
+    GUI_WidgetMarkChanged(self.pages);
 }
 
 static void *app_thread(void *params) {
@@ -56,47 +103,101 @@ static void *app_thread(void *params) {
 
     while(self.app->state & APP_STATE_OPENED) {
         switch(self.action) {
-            case APP_ACTION_CONNECT_BBA:
-                if(dsystem("net --init") == CMD_OK) {
+            case APP_ACTION_ETHERNET:
+                if(self.is_eth_connected) {
+                    GUI_LabelSetTextColor(self.net_status, 207, 51, 17);
+                    self.is_eth_connected = false;
+
+                    if(dsystem("net --shutdown") == CMD_OK) {
+                        GUI_LabelSetText(self.net_status, "Disconnected");
+                    }
+                    else {
+                        GUI_LabelSetText(self.net_status, "Failed");
+                    }
+                }
+                else if(dsystem("net --init") == CMD_OK) {
                     GUI_LabelSetTextColor(self.ip_addr, 51, 51, 51);
                     GUI_LabelSetTextColor(self.net_status, 18, 172, 16);
                     GUI_LabelSetText(self.net_status, "Connected");
+                    self.is_eth_connected = true;
                 }
                 else {
                     GUI_LabelSetText(self.net_status, "Failed");
+                    self.is_eth_connected = false;
                 }
                 GUI_LabelSetText(self.ip_addr, getenv("NET_IPV4"));
+                UpdateButtons();
                 self.action = APP_ACTION_IDLE;
                 break;
-            case APP_ACTION_CONNECT_MODEM:
-                if(dsystem("ppp --init") == CMD_OK) {
+            case APP_ACTION_MODEM:
+                if(self.is_ppp_connected) {
+                    GUI_LabelSetTextColor(self.net_status, 207, 51, 17);
+                    self.is_ppp_connected = false;
+
+                    if(dsystem("ppp --shutdown") == CMD_OK) {
+                        GUI_LabelSetText(self.net_status, "Disconnected");
+                    }
+                    else {
+                        GUI_LabelSetText(self.net_status, "Failed");
+                    }
+                }
+                else if(dsystem("ppp --init") == CMD_OK) {
                     GUI_LabelSetTextColor(self.net_status, 18, 172, 16);
                     GUI_LabelSetText(self.net_status, "Connected");
+                    self.is_ppp_connected = true;
                 }
                 else {
                     GUI_LabelSetText(self.net_status, "Failed");
                 }
                 GUI_LabelSetText(self.ip_addr, getenv("NET_IPV4"));
+                UpdateButtons();
                 self.action = APP_ACTION_IDLE;
                 break;
-            case APP_ACTION_CONNECT_FTPD:
-                if(dsystem("ftpd -s -p 21 -d /") == CMD_OK) {
+            case APP_ACTION_FTPD:
+                if(self.is_ftpd_started) {
+                    GUI_LabelSetTextColor(self.ftpd_status, 207, 51, 17);
+                    self.is_ftpd_started = false;
+
+                    if(dsystem("ftpd -t") == CMD_OK) {
+                        GUI_LabelSetText(self.ftpd_status, "Stopped");
+                        self.is_ftpd_started = false;
+                    }
+                    else {
+                        GUI_LabelSetText(self.ftpd_status, "Failed");
+                    }
+                }
+                else if(dsystem("ftpd -s -p 21 -d /") == CMD_OK) {
                     GUI_LabelSetTextColor(self.ftpd_status, 18, 172, 16);
                     GUI_LabelSetText(self.ftpd_status, "Started, port 21");
+                    self.is_ftpd_started = true;
                 }
                 else {
                     GUI_LabelSetText(self.ftpd_status, "Failed");
                 }
+                UpdateButtons();
                 self.action = APP_ACTION_IDLE;
                 break;
-            case APP_ACTION_CONNECT_HTTPD:
-                if(dsystem("httpd -s -p 80") == CMD_OK) {
+            case APP_ACTION_HTTPD:
+                if(self.is_httpd_started) {
+                    GUI_LabelSetTextColor(self.httpd_status, 207, 51, 17);
+                    self.is_httpd_started = false;
+
+                    if(dsystem("httpd -t") == CMD_OK) {
+                        GUI_LabelSetText(self.httpd_status, "Stopped");
+                    }
+                    else {
+                        GUI_LabelSetText(self.httpd_status, "Failed");
+                    }
+                }
+                else if(dsystem("httpd -s -p 80") == CMD_OK) {
                     GUI_LabelSetTextColor(self.httpd_status, 18, 172, 16);
                     GUI_LabelSetText(self.httpd_status, "Started, port 80");
+                    self.is_httpd_started = true;
                 }
                 else {
                     GUI_LabelSetText(self.httpd_status, "Failed");
                 }
+                UpdateButtons();
                 self.action = APP_ACTION_IDLE;
                 break;
             default:
@@ -108,6 +209,16 @@ static void *app_thread(void *params) {
     return NULL;
 }
 
+static void LoadModuleSafe(const char *name) {
+    if(!GetModuleByName(name)) {
+        char path[NAME_MAX];
+        snprintf(path, sizeof(path), "%s/modules/%s.klf", getenv("PATH"), name);
+
+        if(OpenModule(path)) {
+            listAddItem(self.loaded_modules, LIST_ITEM_MODULE, name, NULL, 0);
+        }
+    }
+}
 
 void NetworkApp_Init(App_t *app) {
 
@@ -124,12 +235,81 @@ void NetworkApp_Init(App_t *app) {
     self.startup_connect_eth_but = APP_GET_WIDGET("startup-connect-eth-but");
     self.startup_connect_ppp_but = APP_GET_WIDGET("startup-connect-ppp-but");
     self.startup_ntp_but = APP_GET_WIDGET("startup-ntp-but");
+    self.keep_running_but = APP_GET_WIDGET("keep-running-but");
 
+    self.eth_but_txt = APP_GET_WIDGET("eth-but-txt");
+    self.ppp_but_txt = APP_GET_WIDGET("ppp-but-txt");
+    self.ftpd_but_txt = APP_GET_WIDGET("ftpd-but-txt");
+    self.httpd_but_txt = APP_GET_WIDGET("httpd-but-txt");
+
+    self.loaded_modules = listMake();
+
+    LoadModuleSafe("ppp");
+    LoadModuleSafe("ftpd");
+    LoadModuleSafe("httpd");
+
+    char *ip = getenv("NET_IPV4");
+
+    if(net_default_dev && (net_default_dev->flags & NETIF_RUNNING)) {
+
+        GUI_LabelSetTextColor(self.ip_addr, 51, 51, 51);
+        GUI_LabelSetTextColor(self.net_status, 18, 172, 16);
+        GUI_LabelSetText(self.net_status, "Connected");
+
+        if(strncmp(net_default_dev->name, "ppp", 3) == 0) {
+            self.is_ppp_connected = true;
+        }
+        else {
+            self.is_eth_connected = true;
+        }
+
+        if(dsystem("ftpd -q") == CMD_OK) {
+            GUI_LabelSetTextColor(self.ftpd_status, 18, 172, 16);
+            GUI_LabelSetText(self.ftpd_status, "Started, port 21");
+            self.is_ftpd_started = true;
+        }
+        if(dsystem("httpd -q") == CMD_OK) {
+            GUI_LabelSetTextColor(self.httpd_status, 18, 172, 16);
+            GUI_LabelSetText(self.httpd_status, "Started, port 80");
+            self.is_httpd_started = true;
+        }
+    }
+    GUI_LabelSetText(self.ip_addr, ip);
+    UpdateButtons();
     SetupNetworkSettings();
 }
 
 void NetworkApp_Shutdown(App_t *app) {
     (void)app;
+
+    if(!GUI_WidgetGetState(self.keep_running_but)) {
+
+        if(self.is_httpd_started) {
+            dsystem("httpd -t");
+        }
+        if(listGetItemByName(self.loaded_modules, "httpd")) {
+            CloseModule(GetModuleByName("httpd"));
+        }
+
+        if(self.is_ftpd_started) {
+            dsystem("ftpd -t");
+        }
+        if(listGetItemByName(self.loaded_modules, "ftpd")) {
+            CloseModule(GetModuleByName("ftpd"));
+        }
+
+        bool ppp_active = false;
+        if(net_default_dev && (net_default_dev->flags & NETIF_RUNNING)) {
+             if(strncmp(net_default_dev->name, "ppp", 3) == 0) {
+                 ppp_active = true;
+             }
+        }
+
+        if(!ppp_active && listGetItemByName(self.loaded_modules, "ppp")) {
+            CloseModule(GetModuleByName("ppp"));
+        }
+    }
+    listDestroy(self.loaded_modules, NULL);
 }
 
 static int SettingsChanged() {
@@ -166,45 +346,37 @@ void NetworkApp_DialogCancel(GUI_Widget *widget) {
 }
 
 void NetworkApp_Open(App_t *app) {
-    (void)app;
-    char *ip = getenv("NET_IPV4");
-    if(strncmp(ip, "0.0.0.0", 7)) {
-        GUI_LabelSetTextColor(self.ip_addr, 51, 51, 51);
-        GUI_LabelSetTextColor(self.net_status, 18, 172, 16);
-        GUI_LabelSetText(self.net_status, "Connected");
-    }
-    GUI_LabelSetText(self.ip_addr, ip);
-    self.app->thd = thd_create(0, app_thread, NULL);
+    app->thd = thd_create(0, app_thread, NULL);
 }
 
-void NetworkApp_ConnectBBA(GUI_Widget *widget) {
+void NetworkApp_ToggleEthernet(GUI_Widget *widget) {
     (void)widget;
     GUI_LabelSetTextColor(self.net_status, 207, 51, 17);
     GUI_LabelSetTextColor(self.ip_addr, 207, 51, 17);
     GUI_LabelSetText(self.net_status, "Connecting...");
-    self.action = APP_ACTION_CONNECT_BBA;
+    self.action = APP_ACTION_ETHERNET;
 }
 
-void NetworkApp_ConnectModem(GUI_Widget *widget) {
+void NetworkApp_ToggleModem(GUI_Widget *widget) {
     (void)widget;
     GUI_LabelSetTextColor(self.net_status, 207, 51, 17);
     GUI_LabelSetTextColor(self.ip_addr, 207, 51, 17);
     GUI_LabelSetText(self.net_status, "Connecting...");
-    self.action = APP_ACTION_CONNECT_MODEM;
+    self.action = APP_ACTION_MODEM;
 }
 
-void NetworkApp_FTP(GUI_Widget *widget) {
+void NetworkApp_ToggleFTP(GUI_Widget *widget) {
     (void)widget;
     GUI_LabelSetTextColor(self.ftpd_status, 207, 51, 17);
     GUI_LabelSetText(self.ftpd_status, "Starting...");
-    self.action = APP_ACTION_CONNECT_FTPD;
+    self.action = APP_ACTION_FTPD;
 }
 
-void NetworkApp_HTTP(GUI_Widget *widget) {
+void NetworkApp_ToggleHTTP(GUI_Widget *widget) {
     (void)widget;
     GUI_LabelSetTextColor(self.httpd_status, 207, 51, 17);
     GUI_LabelSetText(self.httpd_status, "Starting...");
-    self.action = APP_ACTION_CONNECT_HTTPD;
+    self.action = APP_ACTION_HTTPD;
 }
 
 void NetworkApp_ToggleEth(GUI_Widget *widget) {
